af1e80415c94bdff15f971c406afedcbcf7c3452
mayonaiys <49065703+mayonaiys@users.noreply.github.com>
2020-02-13 09:41:21 +0100

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -4,7 +4,10 @@
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
-    QPushButton button ("Hello world !");
+    QPushButton button;
+    button.setText("Ouais le smic");
+    button.setToolTip("Le text");
+    button.setFont()
     button.show();
     return app.exec();
 }
