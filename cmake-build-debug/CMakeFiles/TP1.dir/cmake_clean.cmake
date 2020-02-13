45d8f0995249edd6640ea82da16eceecf53bc5fa
mayonaiys <49065703+mayonaiys@users.noreply.github.com>
2020-02-13 09:33:59 +0100

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/TP1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "TP1_automoc.cpp"
+  "CMakeFiles/TP1.dir/main.cpp.o"
+  "CMakeFiles/TP1.dir/TP1_automoc.cpp.o"
+  "TP1.pdb"
+  "TP1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TP1_automoc.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "TP1_automoc.cpp"
+  "CMakeFiles/TP1_automoc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/TP1_automoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
